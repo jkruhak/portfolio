df43214187bb607f72ca145b62e8ca4a5612ac73
Jasmina Kruhak <jkruhak@gmail.com>
2014-10-13 20:09:06 +0400

    design

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-	
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function(event) {
+	    event.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
