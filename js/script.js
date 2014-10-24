a2a8c8614c28f0c9b92475ac2f1f167ac2de0921
Jasmina Kruhak <jkruhak@gmail.com>
2014-10-25 02:46:32 +0400

    style change

--- js/script.js
+++ js/script.js
@@ -4,24 +4,14 @@ $(document).ready(function(){
 	});
 
 	//animate scroll when clicked on navigation link
-	$('a[href^="#"]').on('click',function(event) {
-	    event.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 1000, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+	navigationLink();
 
 	//Load Shopping List App in window when link clicked
 	$("#projectOneInternal").on("click", function() {
 		projectLoad();
 
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/shoppingListApp/main.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/shoppingListApp/main.html' target='_blank'><h3>Shopping List App</h3></a><br><p>A shopping list to help you remember what to get on your next shopping trip</p>");
 	});
 
 	//Load Fizz Buzz App in window when link clicked
@@ -29,6 +19,7 @@ $(document).ready(function(){
 		projectLoad();
 
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/FizzBuzz2/main.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/FizzBuzz2/main.html' target='_blank'><h3>Fizz Buzz App</h3></a><br><p>A simple app that outputs phrases or numbers based on the input.</p>");
 	});
 
 	//Load Hot or Cold App in window when link clicked
@@ -36,6 +27,7 @@ $(document).ready(function(){
 		projectLoad();
 		
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/hot-or-cold-app/index.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/hot-or-cold-app/index.html' target='_blank'><h3>Hot or Cold App</h3></a><br><p>You against the computer. See how many tries it takes you to guess the number computer has chosen.</p>");
 	});
 
 	//Load Quiz App in window when link clicked
@@ -43,6 +35,7 @@ $(document).ready(function(){
 		projectLoad();
 
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/quizApp/index.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/quizApp/index.html' target='_blank'><h3>Quiz App</h3></a><br><p>A quiz to test your M*A*S*H knowledge.</p>");
 	});
 
 	//Load Stackoverflow Top Answerers App in window when link clicked
@@ -50,6 +43,7 @@ $(document).ready(function(){
 		projectLoad();
 
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/topAnswerers/index.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/topAnswerers/index.html' target='_blank'><h3>Stackoverflow API Hack</h3></a><br><p>Get top answerers for a topic of your choice from Stackoverflow.</p>");
 	});
 
 	//Load API Hack App in window when link clicked
@@ -57,9 +51,16 @@ $(document).ready(function(){
 		projectLoad();
 
 		$("#projectLoaded").html("<object type='text/html' data='http://jkruhak.github.io/apiHack/index.html'></object>");
+		$("#projectName").prepend("<a href='http://jkruhak.github.io/apiHack/index.html' target='_blank'><h3>Weather Station API Hack</h3></a><br><p>Get weather from anywhere in the world with this Weather Station API.</p>");
 	});
 
 	projectClose();
+
+	//close project when clicked anywhere on page
+
+
+	//Back to Top button
+	backToTopButton();
 });
 
 //Animate project loading into window
@@ -75,11 +76,46 @@ var projectLoad = function() {
 
 //Animate when X clicked to close project
 var projectClose = function() {
-	$("#headerProject").on("click", "#xMark", function() {
+	$("#projectName").on("click", "#xMark", function() {
 			
 		$("#projectList").animate({
 		"opacity": "show"}, 1500);
 
 		$("#loadedProject").hide();
+	});
+};
+
+//Back to Top button
+var backToTopButton = function() {
+	var scrollOffset = 100;
+	var duration = 1000;
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() > scrollOffset) {
+			$(".backToTop").fadeIn(duration);
+		} else {
+			$(".backToTop").fadeOut(duration);
+		}
+	});
+
+	$(".backToTop").on("click", function(event) {
+		event.preventDefault();
+		$("html, body").animate({scrollTop: 0}, duration);
+		return false;
+	});
+};
+
+var navigationLink = function() {
+	$('a[href^="#"]:not([href=#])').on('click',function(event) {
+	    event.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
 };
